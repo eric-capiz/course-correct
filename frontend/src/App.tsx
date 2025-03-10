7dd88ce981a0e65232013c0e4166a29a0aa06fa7
eric capiz <ericcapiz@gmail.com>
2025-03-10 12:29:13 -0500

    CHANGES

---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import { Routes, Route } from "react-router-dom";
+import AuthProvider from "@/context/auth/authContext";
+import UserProvider from "@/context/users/userContext";
+import BookingProvider from "@/context/booking/bookingContext";
+import StudyGroupProvider from "@/context/studyGroup/studyGroupContext";
+import TutorAvailabilityProvider from "@/context/tutorAvailability/tutorAvailabilityContext";
+import Navbar from "@/components/global/Navbar";
+import Footer from "@/components/global/Footer";
+import Home from "./app/page";
+import Profile from "./app/profile/page";
+import LearningHub from "./app/learning-hub/page";
+
+function App() {
+  return (
+    <AuthProvider>
+      <UserProvider>
+        <BookingProvider>
+          <StudyGroupProvider>
+            <TutorAvailabilityProvider>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  minHeight: "100vh",
+                }}
+              >
+                <Navbar />
+                <main style={{ flex: "1" }}>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/learning-hub" element={<LearningHub />} />
+                  </Routes>
+                </main>
+                <Footer />
+              </div>
+            </TutorAvailabilityProvider>
+          </StudyGroupProvider>
+        </BookingProvider>
+      </UserProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
