2077428bd3fa55f5985e64bdb249c1ef8b054806
eric capiz <ericcapiz@gmail.com>
2025-03-18 10:52:00 -0500

    backend server frotnend origin adj

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:5173",
       "https://course-correct-gx4f.vercel.app",
       "https://course-correct-gx4f-git-main-eric-capizs-projects.vercel.app",
       "https://course-correct-gx4f-2gpouk1uq-eric-capizs-projects.vercel.app",
